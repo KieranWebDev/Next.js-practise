b09507606edfa271226413809d0ce68f68bc1971
Kieran <kieran.webdevelopment@gmail.com>
2022-07-02 14:34:07 +0100

    fetchedg data using getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,13 +1,32 @@
+import styles from '../../styles/ninjas.module.css';
 import Head from 'next/head';
-function Ninjas() {
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: { ninjas: data },
+  };
+};
+
+function Ninjas({ ninjas }) {
   return (
     <>
       <Head>
         <title>Ninja List | Ninjas</title>
-        <meta> name ='keywords' content = "about"</meta>
+        <meta name="keywords" content="about" />
       </Head>
       <div>
         <h1>All Ninjas</h1>
+
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
