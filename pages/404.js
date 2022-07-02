408aafc84703e460a00aac391896a6c64330eb00
Kieran <kieran.webdevelopment@gmail.com>
2022-07-02 10:24:31 +0100

    added functionality that redirects user from 404 page after 3 seconds. using UseEffect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,14 @@
 import Link from 'next/link';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oooopsss.</h1>
